1eec05076b19fcd8ae6d474e52d3fba61f24848f
yan97ao <magictao@gmail.com>
2014-05-27 14:29:54 +0800

    list directory

--- dir2pic.py
+++ dir2pic.py
@@ -2,12 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from graphviz import Digraph    #pip install graphviz
+import sys
+import os
+
+def dirwalk(path):
+    print path
+    for i in os.listdir(path):
+        fullpath = os.path.join(path,i)
+        print fullpath
+        if os.path.isdir(fullpath):
+            dirwalk(fullpath)
 
 def main() :
-    if(len(sys.argv) <= 1) :
+    if(len(sys.argv) != 2) :
         print "usage:  dir2pic.py PATH"
         return
-
+    path = sys.argv[1]
+    dirwalk(path)
 
 if __name__ == '__main__' :
     main()
