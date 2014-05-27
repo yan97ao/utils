36d17e5b579b540aa9d5e3b818abc7ba7a9eb58e
yan97ao <magictao@gmail.com>
2014-05-27 11:15:02 +0800

    init dir2pic

---
+++ dir2pic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from graphviz import Digraph    #pip install graphviz
+
+def main() :
+    if(len(sys.argv) <= 1) :
+        print "usage:  dir2pic.py PATH"
+        return
+
+
+if __name__ == '__main__' :
+    main()
